5364358941e5ab504e571b9f456c388675ac8406
Julien Moreau <julien@thylo.be>
2023-03-17 11:13:31 +0100

    orbitcontrols

--- src/components/cucumber/Cucumber.tsx
+++ src/components/cucumber/Cucumber.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei'
 
 const Cucumber = () => {
+  const [rotateX, setRotateX] = useState(0);
+
   return (
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
-    </Canvas>
+    <div className="cucumber">
+      <Canvas dpr={[1, 2]} shadows camera={{ position: [-5, 5, 5], fov: 50 }}>
+        <ambientLight intensity={0.1} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 };
 
